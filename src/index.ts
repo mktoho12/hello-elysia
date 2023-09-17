4df2fe98b6588ee590ea68d75c01f9e29413bbaa
mktoho <mk@mktoho.org>
2023-09-18 05:35:51 +0900

    add html plugin

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
-import { Elysia } from "elysia";
+import { html } from '@elysiajs/html'
+import { Elysia } from 'elysia'
+import IndexPage from './pages'
 
 const app = new Elysia()
-  .get('/', () => 'Hello Elysia')
+  .use(html())
+  .get('/', IndexPage)
   .listen(Bun.env.PORT ?? 3000)
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+const IndexPage = () => (
+  <html lang="en">
+    <head>
+      <title>Hello World</title>
+    </head>
+    <body class="abc">
+      <h1>Hello World</h1>
+    </body>
+  </html>
+)
+
+export default IndexPage
